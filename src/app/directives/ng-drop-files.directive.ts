a572c531c4886ddbd07b08f6f1b6b387e1bd2090
Montse Ariso <montxelltech@gmail.com>
2017-07-01 01:29:28 +0200

    Validate the drop of exclusively images adding 'drop' HostListener and some methods

--- src/app/directives/ng-drop-files.directive.ts
+++ src/app/directives/ng-drop-files.directive.ts
@@ -30,7 +30,79 @@ export class NgDropFilesDirective {
 
   @HostListener('dragover', ['$event'])
   public onDragOver( event: any ) {
+
+    let transfer = this._getTransfer(event);
+    transfer.dropEffect = "copy";
+
+    this._preventAndStop(event);
+
     this.fileOn.emit( true );
+  }
+
+
+  @HostListener('drop', ['$event'])
+  public onDrop( event: any ) {
+
+      let transfer = this._getTransfer(event);
+
+      if ( !transfer ) {
+        return;
+      }
+
+      this._addFiles( transfer.files );
+
+      this._preventAndStop(event);
+
+  }
+
+
+
+  private _getTransfer( event: any ) {
+    return event.dataTransfer ? event.dataTransfer : event.originalEvent.dataTransfer;
+  }
+
+
+  private _addFiles( filesList: FileList ) {
+    console.log( filesList );
+  }
+
+
+  private _preventAndStop( event: any ) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+
+  private _fileCanBeUploaded( file: File ) {
+
+    if ( !this._fileAlreadyDropped( file.name ) && this._isImage( file.type) ) {
+      return true;
+    }
+
+    return false;
+  }
+
+
+
+  private _fileAlreadyDropped( fileName: string ): boolean {
+
+    for (let i in this.files ) {
+
+      let registry = this.files[i];
+
+      if ( registry.file.name === fileName ) {
+        console.log("This file already exists in the list: " + fileName );
+        return true;
+      }
+    }
+    return false;
+  }
+
+
+  private _isImage( fileType: string ): boolean {
+
+    return ( fileType == "" || fileType == undefined ) ? false : fileType.startsWith("image");
+
   }
 
 }
