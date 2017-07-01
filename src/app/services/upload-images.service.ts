2d59611512549cc5bd8e96746ea1417e2a18b644
Montse Ariso <montxelltech@gmail.com>
2017-07-01 22:39:33 +0200

    Complete 'uploadImagesToFirebase' method

--- src/app/services/upload-images.service.ts
+++ src/app/services/upload-images.service.ts
@@ -31,6 +31,29 @@ export class UploadImagesService {
 
     console.log( files );
 
+    let storageRef = firebase.storage().ref();
+
+    for ( let item of files ) {
+
+      item.isUploading = true;
+
+      let uploadTask: firebase.storage.UploadTask =
+         storageRef.child(`${ this.IMAGES_FOLDER }/${ item.fileName }`).put( item.file );
+
+      uploadTask.on( firebase.storage.TaskEvent.STATE_CHANGED,
+
+        ( snapshot ) => item.progress = ( snapshot.bytesTransferred / snapshot.totalBytes ) * 100,
+        ( error ) => console.error("Error uploading ", error),
+        ( ) => {
+          item.url = uploadTask.snapshot.downloadURL;
+          item.isUploading = false;
+          this.saveImage( { name: item.fileName, url: item.url} );
+        }
+
+       )
+
+    }
+
   }
 
 
